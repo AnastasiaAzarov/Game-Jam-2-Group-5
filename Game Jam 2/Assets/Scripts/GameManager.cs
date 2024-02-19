2b59a7cf9199e9dfdfc6f1bb581c5890bad11597
GittBitt <nphuynh2@asu.edu>
2024-02-19 15:50:26 -0700

    Added UI elements + Score

--- Game Jam 2/Assets/Scripts/GameManager.cs
+++ Game Jam 2/Assets/Scripts/GameManager.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject obstacle;
     public Transform spawnPoint;
     int score = 0;
+    
+    public TextMeshProUGUI scoreText;
+    public GameObject playButton;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameStart();
+
     }
 
     // Update is called once per frame
@@ -32,9 +37,19 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void ScoreUp()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
     public void GameStart()
     {
+        player.SetActive(true);
+        playButton.SetActive(false);
+
         StartCoroutine("SpawnObstacles");
+        InvokeRepeating("ScoreUp", 2f, 1f);
     }
 
 
